69bb311d83f3739caf8ae87118b1be69d3dbdd25
JasonLiao118 <jack666280@gmail.com>
2022-07-18 22:54:05 -0400

    wasd

--- Project/New.py
+++
@@ -1,8 +0,0 @@
-x = int(input("please put an integer: "))
-error = str("This number is too small")
-nonerror = str("This is a good number")
-
-if x < 0:
-    print(error)
-else:
-    print(nonerror)
