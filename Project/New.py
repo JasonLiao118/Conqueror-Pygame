c1bb0b7e9be5d49ac039910007fa8180400fbaa6
JasonLiao118 <jack666280@gmail.com>
2022-07-22 18:53:42 -0400

    sdawd

---
+++ Project/New.py
@@ -0,0 +1,19 @@
+List = ['a', 'b', 'c']
+
+
+def add():
+    List.append("d"),
+    List.extend(List),
+    print(List)
+
+
+add()
+
+
+Tuple = {1, 2, 3, 4}
+
+Dictionary = {'Jason': 12356, "John": 2457}
+
+Dictionary['Adam'] = 5321
+
+print(Dictionary)
