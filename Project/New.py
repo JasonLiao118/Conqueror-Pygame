d66ad83c12c22d968e746b48b801e865890a38b6
JasonLiao118 <jack666280@gmail.com>
2022-07-22 20:00:27 -0400

    awdasd

--- Project/New.py
+++ Project/New.py
@@ -1,19 +1,3 @@
-List = ['a', 'b', 'c']
+import fibo
 
-
-def add():
-    List.append("d"),
-    List.extend(List),
-    print(List)
-
-
-add()
-
-
-Tuple = {1, 2, 3, 4}
-
-Dictionary = {'Jason': 12356, "John": 2457}
-
-Dictionary['Adam'] = 5321
-
-print(Dictionary)
+new = {fibo.fib(1000)}
