ac26696f61a401bd2164c6afb9f12ecc1d566c93
JasonLiao118 <jack666280@gmail.com>
2022-07-26 19:18:38 -0400

    sA

--- Project/New.py
+++ Project/New.py
@@ -5,12 +5,24 @@ import pygame
 pygame.init()
 
 # Set up the drawing window
-screen = pygame.display.set_mode([500, 500])
+screen = pygame.display.set_mode([800, 600])
 
 #Title and Icon
 pygame.display.set_caption("Basic Game")
+icon = pygame.image.load('character.png')
+pygame.display.set_icon(icon)
 
-# Run until the user asks to quit
+# Player
+playerImg = pygame.image.load('swordsman.png')
+playerX = 170
+playerY = 100
+player_change = 0
+
+
+def player(x, y):
+    screen.blit(playerImg, (x, y))
+
+    # Run until the user asks to quit
 running = True
 while running:
 
@@ -22,8 +34,7 @@ while running:
     # Fill the background with white
     screen.fill((255, 255, 255))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+    player(playerX, playerY)
 
     # Flip the display
     pygame.display.flip()
