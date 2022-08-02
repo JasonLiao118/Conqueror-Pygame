e68a09d2426f09cfab398c3fb30bcb0c990f8859
JasonLiao118 <jack666280@gmail.com>
2022-08-02 19:46:01 -0400

    wdasd

---
+++ Project/code/gamedata.py
@@ -0,0 +1,8 @@
+level_0 = {
+    'terrain': '../Level 0_terrain.csv',
+    'player': '../Level 0_player.csv',
+    'enemies': '../Level 0_enemies.csv',
+    'decorations': '../Level 0_decorations.csv',
+    'constraints': '../Level 0_constraints.csv',
+    'background': '../Level 0_background.csv'
+}
--- Project/gamedata.py
+++
@@ -1,8 +0,0 @@
-level_0 = {
-    'terrain': './levels/0/Level_0_terrain.csv',
-    'player': './levels/0/Level_0_player.csv',
-    'enemies': './levels/0/Level_0_enemies.csv',
-    'decorations': './levels/0/Level_0_decorations.csv',
-    'constraints': './levels/0/Level_0_constraints.csv',
-    'background': './levels/0/Level_0_background.csv'
-}
